08b1bd52b78fd23aee80c3c96a2cb34a43450b64
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-31 20:24:31 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,18 +1,19 @@
 require "fileutils"
-require "dir"
 require "os"
 require "securerandom"
 
 OS.windows? ? sep = "\\" : sep = "/"  
-cwd = dir::getwd()
+cwd = FileUtils.pwd()
 
 count = 0
 
 while true
     name = SecureRandom.hex
     Sleep(1)
-    FileUtils.mkdir(
+    FileUtils.mkdir("#{cwd}#{sep}#{name}#{count}")
     count += 1
 
+    if count == 10
+        all =
 for dir in cwd
     #
